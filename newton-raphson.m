f99f21a0c71914558c13da146cf547fcc6539878
BrunoGomesCoelho <brasinhacoelho@hotmail.com>
2018-07-03 14:26:45 -0300

    Refactor code

--- newton-raphson.m
+++ newton-raphson.m
@@ -1,43 +1,26 @@
-function [Raiz,Iter,Erro] = Newton_Raiz(funcao,x0,Toler,IterMax);
-    %parametros de entrada: funcao,x0,Toler,IterMax -> funcao, valor inicial,
-    %tolerancia e num. max. de iteracoes
-    %parametros de saida: Raiz,Iter,Erro-> raiz, num. gasto de iteracoes e
-    %cond. de erro
-
-
-    x = x0;
+function [Raiz,Iter] = Newton_Raiz(funcao, Toler);
+    """
+    Calcula a raiz esperada utilizando o método de Newton.
+    Retorna a raiz encontrada e a quantidade de itereções necessárias.
+    """
+    
+    % Inicialmente o x0 é de 0.5
+    x = 0.5;
     Fx = eval(funcao);
     DFx = eval(diff(funcao));
     Iter = 0;
-    disp('      Iter        x     DFx      Fx        DeltaX');
-    disp([Iter           x        DFx      Fx     ]);
-
-    while (1)
-        DeltaX = -Fx/DFx;
-        x = x+DeltaX;
+   
+    while (True)
+        delta_x = -Fx / DFx;
+        x = x + delta_x;
         Fx = eval(funcao);
         DFx = eval(diff(funcao));
-        Iter = Iter+1;
-        disp([Iter     x     DFx      Fx     DeltaX]);       
-        if(abs(DeltaX)<Toler && abs(Fx)<Toler)||abs(DFx)==0 || Iter>= IterMax
+        Iter = Iter + 1;
+        if( abs(delta_x) < tolerancia && abs(Fx)<tolerancia) || abs(DFx) == 0
             break;
         end     
     end
 
     Raiz = x;
-
-    if abs(Fx)<Toler
-        Erro = 0;
-    else
-        Erro =1;
-    end
-
-    y = [];
-    for x=0.001:0.001:10
-        y = [y,eval(funcao)];
-    end
-
-    x = 0.001:0.001:10;
-    plot(x,y);
-
+  
 end
