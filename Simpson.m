dfbfa1484be11f5bae4ed324e4d094ae80ebd0d8
BrunoGomesCoelho <brasinhacoelho@hotmail.com>
2018-07-03 18:25:14 -0300

    Fix various coding issues

--- Simpson.m
+++ Simpson.m
@@ -1,22 +1,23 @@
-function [x,fx,v] = Simpson(a,b,n,func)
-   
-    h = (b-a)/n;
-    x = a:((b-a)/n):b;
+function [x,fx,v] = Simpson(a,b,n,funcao)
+  
+    format long # Para melhor a precisao
+    
+    tam_passo = (b-a) / n;
+    x = a: ((b-a)/n) :b;
 
-    func = inline(func);
+    funcao = inline(funcao);
     for i = 1:length(x)
-        fx(i) = func(x(i));
+        fx(i) = funcao(x(i));
     end
 
-    v=fx(1)+fx(length(x));
+    v = fx(1) + fx(length(x));
     for i= 2:length(x)-1
         if mod(i,2) == 0
-            v+= 4*fx(i); 
+            v += 4 * fx(i); 
         else
-            v+= 2*fx(i);   
+            v += 2 * fx(i);   
         end 
     end
 
-    v = h/3*v;
-
-endfunction
+    v = tam_passo /3*v;
+end
--- newton-raphson.m
+++ newton-raphson.m
@@ -1,26 +1,25 @@
-function [Raiz,Iter] = Newton_Raiz(funcao, Toler);
+function [resposta] = met_newton(funcao, tolerancia);
     """
-    Calcula a raiz esperada utilizando o método de Newton.
-    Retorna a raiz encontrada e a quantidade de itereções necessárias.
+    Calcula uma aproximação para a raiz de uma função 
+      utilizando o metodo de Newton.
     """
     
-    % Inicialmente o x0 é de 0.5
-    x = 0.5;
+    format long # Para melhor a precisao
+
+    x = 0.5; % Inicialmente o x0 eh de 0.5
     Fx = eval(funcao);
     DFx = eval(diff(funcao));
-    Iter = 0;
    
     while (True)
         delta_x = -Fx / DFx;
         x = x + delta_x;
         Fx = eval(funcao);
         DFx = eval(diff(funcao));
-        Iter = Iter + 1;
-        if( abs(delta_x) < tolerancia && abs(Fx)<tolerancia) || abs(DFx) == 0
+        if (abs(delta_x) < tolerancia && abs(Fx)<tolerancia) || abs(DFx) == 0
             break;
         end     
     end
 
-    Raiz = x;
+    resposta = x;
   
 end
